7574789a2c0b928ddb560664f90dfa7d60606c9b
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-23 22:12:31 +0800

    Removed HTML Tags

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>File Upload</title>
-</head>
-<body>
-    <!-- 
-        <form action="/?action=upload" enctype="multipart/form-data" method="post">
-            <input type="text" name="thesis_title"><br>
-            <input type="file" name="thesis" id="thesis"><br>
-            <button name="submit">Upload</button>
-        </form>
-    -->
+
 
     <?php 
         if(isset($_GET["action"])) {
@@ -68,6 +53,3 @@
             }
         }
     ?>
-
-</body>
-</html>
